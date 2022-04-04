eebe81e94ab65b8df1358e2fa502f504210aec1a
cleofan <58447038+cleofan@users.noreply.github.com>
2022-04-04 13:03:36 -0400

    Update ZK_equality.py

--- ZK_equality.py
+++ ZK_equality.py
@@ -7,7 +7,7 @@ def ZK_equality(G,H):
     r1 = Secret(utils.get_random_num(bits=128))
     r2 = Secret(utils.get_random_num(bits=128))
 
-    m=1
+    m=Secret(utils.get_random_num(bits=128))
     C1 = r1.value * G
     C2 = m * G + r1.value * H
 
